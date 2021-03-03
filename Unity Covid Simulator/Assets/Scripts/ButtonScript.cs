e8b8487431d7f2bbcb9271d5b1544e5360804c57
TrevonPhillip868 <63053149+TrevonPhillip868@users.noreply.github.com>
2021-03-03 09:53:19 -0400

    Add files via upload

---
+++ Unity Covid Simulator/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject Human;
+    public HumanScript script;
+    public Text RateText;
+    public Transform startpoint;
+    public bool socialdistancing = false;
+    public bool mask = false;
+
+    public void OnAddButtonPress(){
+        GameObject duplicate = Instantiate(Human, startpoint.position, startpoint.rotation);
+    }
+
+    public void OnSDButtonPress(){
+        if(socialdistancing == false){
+            socialdistancing = true;
+            ChangeRate();
+        }
+        else{
+            socialdistancing = false;
+            ChangeRate();
+        }
+    }
+
+    public void OnMaskButtonPress(){
+        if(mask == false){
+            mask = true;
+            ChangeRate();
+        }
+        else{
+            mask = false;
+            ChangeRate();
+        }
+    }    
+
+    public void ChangeRate(){
+        HumanScript hscript = Human.gameObject.GetComponent<HumanScript>();
+        if (mask == true && socialdistancing == true){
+            hscript.RateChange(4);
+            RateText.text ="Current Rate: 0.4%";
+        }
+        else if (mask == true && socialdistancing == false){
+            hscript.RateChange(31);
+            RateText.text ="Current Rate: 3.1%";
+        }
+        else if (mask == false && socialdistancing == true){
+            hscript.RateChange(26);
+            RateText.text ="Current Rate: 2.6%";
+        }
+        else if (mask == false && socialdistancing == false){
+            hscript.RateChange(174);
+            RateText.text ="Current Rate: 17.4%";
+        }
+    }    
+}
---
+++ Unity Covid Simulator/Assets/Scripts/HumanScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanScript : MonoBehaviour
+{
+    public int currentrate;
+    public GameObject Human;
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+    public bool infection;
+    private SpriteRenderer rend;
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+    void Start(){
+
+        randomSpot = Random.Range(0, moveSpots.Length);
+        currentrate = 174;
+        infection = false;
+    }
+
+    public void OnTriggerEnter2D(Collider2D col){
+            HumanScript hscript = col.gameObject.GetComponent<HumanScript>();
+            if (hscript.infection == true){
+                int x = Random.Range(0, 1000);
+                if (x <=currentrate && x >= 0){
+                    infection = true;
+                    rend = GetComponent<SpriteRenderer>();
+                    rend.material.color = Color.red;
+                }
+            }    
+    }
+
+    void OnMouseDown(){
+            if (infection == true){
+                infection = false;
+                rend = GetComponent<SpriteRenderer>();
+                rend.material.color = Color.white;
+            }
+            else {
+                infection = true;
+                rend = GetComponent<SpriteRenderer>();
+                rend.material.color = Color.red;
+            }          
+    }
+
+    public void RateChange (int rate){
+        currentrate = rate;
+    }
+
+    void Update(){
+
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f){
+            if (waitTime <= 0){
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+        else if(Vector2.Distance(transform.position, moveSpots[8].position) < 0.2f){
+                Destroy(Human);
+            }
+            else{
+                waitTime  -= Time.deltaTime;
+            }
+        }
+    }
+}
